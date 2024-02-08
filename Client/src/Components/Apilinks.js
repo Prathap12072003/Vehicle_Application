74d45a55ecc670a5fc75980541ec61b8486764cf
Prathap12072003 <prathap60647@gmail.com>
2024-02-08 12:02:26 +0530

    modified backend url

--- Client/src/Components/Apilinks.js
+++ Client/src/Components/Apilinks.js
@@ -1,9 +1,9 @@
-export const Base_Url=`http://localhost:5000/vehicle`
+export const Base_Url=`https://vehilceinfoapplication-backend.onrender.com/vehicle`
 
-export const PostURL='http://localhost:5000/vehicle/vehiclesdetails/post'
+export const PostURL='https://vehilceinfoapplication-backend.onrender.com/vehicle/vehiclesdetails/post'
 
-export const GetURL='http://localhost:5000/vehicle/vehiclesdetails'
+export const GetURL='https://vehilceinfoapplication-backend.onrender.com/vehicle/vehiclesdetails'
 
 // export const Url='https://64e88b6999cf45b15fdfbea2.mockapi.io/data'
 
-export const deleteURL='http://localhost:5000/vehicle/delete'
+export const deleteURL='https://vehilceinfoapplication-backend.onrender.com/vehicle/delete'
