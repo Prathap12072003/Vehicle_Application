3f506f227e60e5518f033f2aaf6028873482eff6
Prathap12072003 <prathap60647@gmail.com>
2024-06-23 10:49:46 +0530

    updated backend url

--- Client/src/Components/Apilinks.js
+++ Client/src/Components/Apilinks.js
@@ -1,9 +1,9 @@
-export const Base_Url=`https://vehilceinfoapplication-backend.onrender.com/vehicle`
+export const Base_Url=`https://vehicle-application-xpcy.onrender.com/vehicle`
 
-export const PostURL='https://vehilceinfoapplication-backend.onrender.com/vehicle/vehiclesdetails/post'
+export const PostURL='https://vehicle-application-xpcy.onrender.com/vehicle/vehiclesdetails/post'
 
-export const GetURL='https://vehilceinfoapplication-backend.onrender.com/vehicle/vehiclesdetails'
+export const GetURL='https://vehicle-application-xpcy.onrender.com/vehicle/vehiclesdetails'
 
 // export const Url='https://64e88b6999cf45b15fdfbea2.mockapi.io/data'
 
-export const deleteURL='https://vehilceinfoapplication-backend.onrender.com/vehicle/delete'
+export const deleteURL='https://vehicle-application-xpcy.onrender.com/vehicle/delete'
